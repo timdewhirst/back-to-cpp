904e1eda0f4ec57c77d2db37af47fc015afd5f5a
alwaysfish <ed091maf@gmail.com>
2020-11-03 13:31:32 +0000

    Updated ExchangeRate class

--- ed/fx_converter/exchange_rate.cpp
+++ ed/fx_converter/exchange_rate.cpp
@@ -1,9 +1,20 @@
 #include "exchange_rate.hpp"
 
-ExchangeRate::ExchangeRate(const Currency& base, const Currency& quote, double rate)
+ExchangeRate::ExchangeRate(const string& base, const string& quote, double bid, double ask) :
+    m_base(base), m_quote(quote), m_bid(bid), m_ask(ask) {}
+
+double ExchangeRate::buy(double amount)
 {
-    m_base = &base;
-    m_quote = &quote;
-    m_rate = rate;
+    return amount * m_ask;
+}
+    
+double ExchangeRate::sell(double amount)
+{
+    return amount * m_bid;
 }
 
+void ExchangeRate::update(double bid, double ask)
+{
+    if (bid != -1) m_bid = bid;
+    if (ask != -1) m_ask = ask;
+}
