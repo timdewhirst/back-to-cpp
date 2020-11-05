5c24a3bad325deef08fae9596b86071a6432a20b
alwaysfish <ed091maf@gmail.com>
2020-11-05 20:29:24 +0000

    Prototype of ExchangeBoard

--- ed/fx_converter/exchange_board.hpp
+++ ed/fx_converter/exchange_board.hpp
@@ -1,12 +1,21 @@
-#pragma once
+#ifndef EXCHANGE_BOARD_HPP
+#define EXCHANGE_BOARD_HPP
+
 #include <string>
+#include <unordered_map>
+#include "exchange_rate.hpp"
 
 using namespace std;
 
 class ExchangeBoard
 {
 private:
+    unordered_map<string, unordered_map<string, const ExchangeRate*>> m_board;
     
 public:
+    void add_rates(const unordered_map<string, ExchangeRate>& rates);
+    
     double convert(const string& from, const string& to, double amount);
 };
+
+#endif
